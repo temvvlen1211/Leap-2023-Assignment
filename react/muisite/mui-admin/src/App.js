01ea754caaa036c7b45f3b98a936d718edf99884
Evening <evening@mstars-lab2-14s-iMac.local>
2023-02-14 15:43:49 +0800

    mui toggle

---
+++ react/muisite/mui-admin/src/App.js
@@ -0,0 +1,24 @@
+import { Box, Button } from "@mui/material";
+import { Layout } from "./components/Layout";
+import { useToast } from "./hooks";
+
+function App() {
+  const showToast = useToast();
+
+  return (
+    <Layout>
+      <Box sx={{ p: 5 }}>
+        <Button
+          variant="contained"
+          onClick={() => {
+            showToast("!!!ALDAA GARCHAAD BN HOOY!!! ");
+          }}
+        >
+          Toggle toast
+        </Button>
+      </Box>
+    </Layout>
+  );
+}
+
+export default App;
